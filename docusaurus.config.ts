6e7f5eedcfb7d2fcca1cf59414aed25f3e1d6985
Jacek Janczura <jacek.janczura@jadenx.com>
2024-03-27 14:34:47 +0100

    initial commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,131 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'entAIngine documentation',
+  tagline: 'Documentation of entAIngine - Enterprise AI Engine',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'entAIngine', // Usually your GitHub org/user name.
+  projectName: 'entaingine-docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/entAIngine/entaingine-docs/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: false,
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/entAIngine/entaingine-docs/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'entAIngine',
+      logo: {
+        alt: 'entAIngine Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        // {
+        //   type: 'docSidebar',
+        //   sidebarId: 'tutorialSidebar',
+        //   position: 'left',
+        //   label: 'Tutorial',
+        // },
+        // {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://jadenx.atlassian.net/servicedesk/customer/portals',
+          label: 'Help Desk',
+          position: 'right',
+        },
+        {
+          href: 'https://entaingine.statuspage.io/',
+          label: 'Health Status',
+          position: 'right',
+        }
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'YouTube',
+              href: 'https://www.youtube.com/@entAIngine-xs2db',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/HSApqmUG',
+            },
+            {
+              label: 'LinkedIn',
+              href: 'https://www.linkedin.com/company/entaingine/',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/entaingine',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Jaden Data, GmbH. Built by entAIngine.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
